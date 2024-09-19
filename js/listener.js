3f855d20433af63eadec59e2debd601046fe6988
juanpablofuentes <incidencias@gmail.com>
2024-09-19 10:11:26 +0200

    update listener

--- js/listener.js
+++ js/listener.js
@@ -7,7 +7,7 @@ caja.addEventListener("click",mostrar);
 nombre.addEventListener("keydown",tecla);
 
 caja.addEventListener("click",function(){
-    console.log("Esto se ejecuta también");
+    console.log("Esto se ejecuta por que sí");
 });
 
 caja.addEventListener("click",(evt)=>{
